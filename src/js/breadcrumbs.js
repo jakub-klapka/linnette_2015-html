cb9051ec944c19855adedd1ac816750b92f894bc
Jakub Klapka <klapka@lumiart.cz>
2015-03-16 16:11:45 +0100

    Add breadcrumbs

---
+++ src/js/breadcrumbs.js
@@ -0,0 +1,31 @@
+/* global jQuery */
+( function( $ ){
+
+	var Breadcrumbs = {
+
+		init: function( el ) {
+
+			this.el = el.children();
+
+			this.scrollToEnd();
+			$( document ).load( $.proxy( this.scrollToEnd, this ) );
+			$( window ).on( 'resize', $.proxy( this.scrollToEnd, this ) );
+
+		},
+
+		scrollToEnd: function() {
+
+			console.log(this.el.outerWidth( true ));
+			this.el.scrollLeft( 9999 );
+
+		}
+
+	};
+
+	$( function() {
+		$( '[data-js-breadcrumbs]' ).each( function() {
+			Object.create( Breadcrumbs ).init( $( this ) );
+		} );
+	} );
+
+} )( jQuery );
