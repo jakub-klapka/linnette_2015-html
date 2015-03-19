3ee9d227b2db6bf3838282aa59bc599bbe5072f4
Jakub Klapka <klapka@lumiart.cz>
2015-03-19 21:23:37 +0100

    Add lightbox

---
+++ src/js/lightbox.js
@@ -0,0 +1,42 @@
+/* global jQuery */
+( function( $ ){
+
+	$( function(){
+
+		var figures = $( '.article_gallery figure' );
+
+		figures.on( 'click', function(){
+
+			var index = figures.index( $( this ) );
+
+			var psitems = [];
+
+			figures.each( function(){
+				var t = $( this ),
+					img = t.find( 'img' );
+				psitems.push( {
+					src: img.data( 'url' ),
+					w: img.data( 'width' ),
+					h: img.data( 'height' ),
+					title: t.find( 'figcaption' ).text()
+				} );
+
+			} );
+
+			var options = {
+				index: index,
+				showHideOpacity: true,
+				loop: false,
+				shareEl: false
+			};
+
+			var gallery = new PhotoSwipe( document.querySelectorAll('.pswp')[0], PhotoSwipeUI_Default, psitems, options );
+			gallery.init();
+
+		} );
+
+
+
+	} );
+
+} )( jQuery );
