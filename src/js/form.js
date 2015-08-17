6c589ecd4f4dd81198aa04456cd2eabb24a2e0bd
Jakub Klapka <klapka@lumiart.cz>
2015-08-17 18:43:00 +0200

    Basic comment styles

--- src/js/form.js
+++ src/js/form.js
@@ -1,9 +1,41 @@
 /* global jQuery, autosize */
 ( function( $ ){
 
+	var AutoShowing = {
+
+		init: function( el ) {
+
+			this.source = el;
+			this.target = $( '[data-js-hidden_target=' + this.source.data( 'js-hidden_source' ) + ']' );
+			this.hidden = true;
+
+			this.bindEvents();
+		},
+
+		bindEvents: function() {
+
+			this.source.on( 'focus', $.proxy( this.showTarget, this ) );
+
+		},
+
+		showTarget: function() {
+			if( this.hidden === true ) {
+				this.target.velocity( 'slideDown' );
+				this.hidden = false;
+			}
+		}
+
+	};
+
+
+	//DOM ready
 	$( function() {
 
 		autosize( $( '.form textarea' ) );
+
+		$( '[data-js-hidden_source]' ).each( function(){
+			Object.create( AutoShowing ).init( $( this ) );
+		} );
 
 	} );
 
