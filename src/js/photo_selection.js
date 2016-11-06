3c8c917c85bb7f0fef114f097e5afd054500a7ec
Jakub Klapka <klapka@lumiart.cz>
2016-11-06 19:50:47 +0100

    Init PSWP gallery and button/checkbox events handling

--- src/js/photo_selection.js
+++ src/js/photo_selection.js
@@ -18,6 +18,21 @@
 		labels: undefined,
 
 		/**
+		 * All Figures in current gallery
+		 */
+		figures: undefined,
+
+		/**
+		 * Instance of Photoswipe, once initialized
+		 */
+		photoswipe: undefined,
+
+		/**
+		 * Button inside PSWP, which selects images
+		 */
+		pswpSelectButton: undefined,
+
+		/**
 		 * Constructor, fired for each gallery
 		 *
 		 * @param wraper jQuery
@@ -29,12 +44,15 @@
 			this.wraper = wraper;
 			this.checkboxes = wraper.find( '.photo_selection__checkbox' );
 			this.labels = wraper.find( '.photo_selection__checkbox__label' );
+			this.figures = wraper.find( '.images_list__item' );
+			this.pswpSelectButton = $( '.pswp__linnete-photo-selection__button' );
 
 			/**
 			 * Events
 			 */
 			this.removeCheckboxClickBubble();
 			this.toggleWrapClassOnCheck();
+			this.bindPswpEvents();
 
 		},
 
@@ -61,6 +79,120 @@
 					parent_wrap.removeClass( 'is-checked' )
 				}
 			} );
+		},
+
+		/**
+		 * Bind events, which are related to PSWP start
+		 */
+		bindPswpEvents: function () {
+			var t = this;
+
+			//Click on figure to init photoswipe
+			this.figures.on( 'click', function(){
+				t.initPswp( $( this ) );
+			} );
+
+			//Click on photo select button in PS
+			this.pswpSelectButton.on( 'click', $.proxy( this.passButtonClickToCheckbox, this ) );
+
+		},
+
+		/**
+		 * Init and start photoswipe gallery
+		 *
+		 * @event click on figure
+		 * @param clicked_figure
+		 */
+		initPswp: function( clicked_figure ) {
+
+			var index = this.figures.index( clicked_figure );
+
+			var psitems = [];
+
+			this.figures.each( function(){
+				var t = $( this ),
+					img = t.find( 'img' );
+				psitems.push( {
+					src: img.data( 'url' ),
+					w: img.data( 'width' ),
+					h: img.data( 'height' ),
+					title: t.find( 'figcaption' ).text(),
+					id: t.find( '.photo_selection__checkbox' ).prop( 'id' )
+				} );
+			} );
+
+			var options = {
+				index: index,
+				showHideOpacity: true,
+				loop: false,
+				shareEl: false
+			};
+
+			this.photoswipe = new PhotoSwipe( document.querySelectorAll('.pswp')[0], PhotoSwipeUI_Default, psitems, options );
+			/*
+			 * PSWP Events
+			 */
+			this.photoswipe.listen( 'beforeChange', $.proxy( this.setPswpImageSelectorValue, this ) );
+			this.bindWindowXKeySelections( true );
+			this.photoswipe.listen( 'unbindEvents', $.proxy( this.bindWindowXKeySelections, this, false ) );
+
+			this.photoswipe.init();
+		},
+
+		/**
+		 * Bind X key press to (de)select picture in PSWP
+		 *
+		 * @param on if true, we are binding event, if false, we are unbinding (gallery destroy)
+		 */
+		bindWindowXKeySelections: function( on ) {
+			on = ( typeof on === 'undefined' ) ? false : true;
+
+			if( on ) {
+				//Press X in PS window to select picture
+				$( document ).on( 'keypress.linn__ps_select_picture', $.proxy( function ( event ) {
+					if( event.which === 120 ) {
+						//X pressed
+						this.passButtonClickToCheckbox();
+					}
+				}, this ) );
+			} else {
+				$( document ).off( 'keypress.linn__ps_select_picture' );
+			}
+
+		},
+
+		/**
+		 * On every photo change in PSWP, find related checkbox and set button inside pswp to correct value
+		 *
+		 * @event photoswipe.beforeChange
+		 */
+		setPswpImageSelectorValue: function () {
+
+			var checkbox = this.wraper.find( '#' + this.photoswipe.currItem.id );
+			if( checkbox.is( ':checked' ) ) {
+				this.pswpSelectButton.addClass( 'is-selected' );
+			} else {
+				this.pswpSelectButton.removeClass( 'is-selected' );
+			}
+
+		},
+
+		/**
+		 * Handle PSWP select button click -> pass clicked value back to related checkbox in DOM
+		 */
+		passButtonClickToCheckbox: function () {
+
+			var checkbox = this.wraper.find( '#' + this.photoswipe.currItem.id );
+			if( !this.pswpSelectButton.hasClass( 'is-selected' ) ) {
+				//Select checkbox
+				checkbox.prop( 'checked', true ).trigger( 'change' );
+				this.pswpSelectButton.addClass( 'is-selected' );
+			} else {
+				//Deselect checkbox
+				checkbox.prop( 'checked', false ).trigger( 'change' );
+				this.pswpSelectButton.removeClass( 'is-selected' );
+			}
+
 		}
 
 	};
